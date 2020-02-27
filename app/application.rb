a3d53e66821224dc751c042d596786ebb4137ac3
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-27 22:56:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,17 @@ class Application
     resp = Rack::Response.new
 
     time_of_day = Time.now.hour
+    
+    morning_message = resp.write "Good Morning!"
+    afternoon_message = resp.write "Good Afternoon!"
 
-    if time_of_day <= 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
+    time_of_day <= 12 ? morning_message : afternoon_message
 
+    # if time_of_day <= 12
+    #   resp.write "Good Morning!"
+    # else
+    #   resp.write "Good Afternoon!"
+    # end
     resp.finish
   end
 end
