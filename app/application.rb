49a57384f835f49ee35be9d19a892e8522fb0928
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-27 22:54:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     time_of_day = Time.now.hour
 
-    if time_of_day >= 12
+    if time_of_day <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
