839417f63aeb8250e4de655fdfd3f63460404bc7
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-27 22:56:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
 
     time_of_day = Time.now.hour
-    
+
     morning_message = resp.write "Good Morning!"
     afternoon_message = resp.write "Good Afternoon!"
 
